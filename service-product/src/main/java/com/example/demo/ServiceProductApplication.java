097d1dcf8b26ea1d1d50d78c5ce8673a5eb19f18
Waterstrong <sqlin@thoughtworks.com>
2017-06-29 19:27:54 +0800

    update service product

--- service-product/src/main/java/com/example/demo/ServiceProductApplication.java
+++ service-product/src/main/java/com/example/demo/ServiceProductApplication.java
@@ -1,17 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ServiceProductApplication {
 
+    @Value("${config.message: no data}")
+    private String message;
+
+    @RequestMapping("/hello")
+    String hello() {
+        return message;
+    }
+
+    HelloWorld helloWorld = new HelloWorld(message);
+
+    @RequestMapping("/world")
+    String world() {
+        return helloWorld.getMessage();
+    }
     @RequestMapping("/categories")
     List<Category> categories() {
         return Arrays.asList(
@@ -25,6 +42,17 @@ public class ServiceProductApplication {
     }
 }
 
+class HelloWorld {
+    private String message;
+
+    public HelloWorld(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
 class Category {
     private Integer id;
     private String categoryName;
