f8abb92c5711faf10d82a924f87ebc3295aeffa8
Waterstrong <sqlin@thoughtworks.com>
2017-06-29 19:30:05 +0800

    update src

---
+++ src/main/java/com/example/demo/InfraEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraEurekaServerApplication.class, args);
+	}
+}
